fb987733cdbb69ee7c2298ba954c03fe3c2ad865
dishant <sapla1125@gmail.com>
2022-01-08 21:05:58 -0500

    Adding Fetch Requests and Styling Each Row

---
+++ src/Row.js
@@ -0,0 +1,33 @@
+import React, { useState, useEffect } from 'react';
+import axios from './axios';
+import "./Row.css";
+const base_url = "https://image.tmdb.org/t/p/original/";
+
+export default function Row({ title,fetchUrl }) {
+    const [movies, setMovies] = useState([]);
+
+    useEffect(() => {
+        async function fetchData(){
+            const request = await axios.get(fetchUrl);
+            setMovies(request.data.results);
+            return request;
+        } fetchData();
+    }, [fetchUrl]);
+    
+    return (
+        <div className="row">
+            <h2>{title}</h2>
+            <div className="rows__posters">
+                {/* Several Row Posters */}
+
+                {movies.map(movie => (
+                    <img 
+                    key = {movie.id}
+                    className="row__poster"
+                    src={`${base_url}${movie.poster_path}`} 
+                    alt={movie.name} />
+                ))}
+            </div>
+        </div>
+    )
+}
