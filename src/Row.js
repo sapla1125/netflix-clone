bf8a15eef2d80135b497b676479a10e89478dce5
dishant <sapla1125@gmail.com>
2022-01-10 13:48:49 -0500

    Adding On Click view youtube trailor functionality. Using react-youtube. Plays for some movies it can actually search name for

--- src/Row.js
+++ src/Row.js
@@ -1,10 +1,16 @@
 import React, { useState, useEffect } from 'react';
 import axios from './axios';
 import "./Row.css";
+import Youtube from "react-youtube";
+import movieTrailer from 'movie-trailer';
+
+
 const base_url = "https://image.tmdb.org/t/p/original/";
+
 
 export default function Row({ title,fetchUrl, isLargeRow }) {
     const [movies, setMovies] = useState([]);
+    const [trailerUrl, setTrailorUrl] = useState("");
 
     useEffect(() => {
         async function fetchData(){
@@ -13,6 +19,28 @@ export default function Row({ title,fetchUrl, isLargeRow }) {
             return request;
         } fetchData();
     }, [fetchUrl]);
+    
+    const opts = {
+        height: "390",
+        width: "100%",
+        playerVars:{
+            autoplay:1,
+        }
+    }
+
+    const handleClick = (movie) => {
+        if(trailerUrl){
+            setTrailorUrl('');
+        }else {
+            movieTrailer(movie?.name || "")
+            .then((url) => {
+                const urlParams = new URLSearchParams (new URL(url).search);
+                setTrailorUrl(urlParams.get('v')); 
+            })
+            .catch( (error) => console.log(error));
+        }
+    }
+    
     
     return (
         <div className="row">
@@ -23,11 +51,13 @@ export default function Row({ title,fetchUrl, isLargeRow }) {
                 {movies.map(movie => (
                     <img 
                     key = {movie.id}
+                    onClick={() => handleClick(movie)}
                     className={`row__poster ${isLargeRow && "row_posterLarge"}`}
                     src={`${base_url}${isLargeRow ? movie.poster_path : movie.backdrop_path}`} 
                     alt={movie.name} />
                 ))}
             </div>
+            {trailerUrl && <Youtube videoId={trailerUrl} opts={opts} /> }
         </div>
     )
 }
