a3c2e43406cf0282d3387356d5ad672a6ce3c756
dishant <sapla1125@gmail.com>
2022-01-10 11:44:12 -0500

    Adding Banner Image fetching from API and Styling

--- src/Row.js
+++ src/Row.js
@@ -3,7 +3,7 @@ import axios from './axios';
 import "./Row.css";
 const base_url = "https://image.tmdb.org/t/p/original/";
 
-export default function Row({ title,fetchUrl }) {
+export default function Row({ title,fetchUrl, isLargeRow }) {
     const [movies, setMovies] = useState([]);
 
     useEffect(() => {
@@ -23,8 +23,8 @@ export default function Row({ title,fetchUrl }) {
                 {movies.map(movie => (
                     <img 
                     key = {movie.id}
-                    className="row__poster"
-                    src={`${base_url}${movie.poster_path}`} 
+                    className={`row__poster ${isLargeRow && "row_posterLarge"}`}
+                    src={`${base_url}${isLargeRow ? movie.poster_path : movie.backdrop_path}`} 
                     alt={movie.name} />
                 ))}
             </div>
